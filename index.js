db216abad88b1ff88fd0e145dbf7c07075873edf
laptop <ahad.yekta2004@gmail.com>
2017-08-04 12:19:56 +0430

    third

--- index.js
+++ index.js
@@ -14,14 +14,9 @@ var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
 
-    var config = {
-     clientId     : "506758245884-96no3ods579omcjsommu5d64e2e3bhkr.apps.googleusercontent.com",
-     clientSecret : "WTZWOGLRNdS0iTJNCEyWtaPA",
-     redirectURI  : "http://localhost:4000/callback"
-    }
+ var dataConfig = fs.readFileSync('./config.json', 'utf8');
+ var config = JSON.parse(dataConfig); //now it an object
 
-    var access = 'ya29.GlubBBDMxtFZedjL2ploIepZYNXuSJbIhKHNu4Tg0MCOkmT5xy08uSRUlRZQXbnD9lrkaoEtznoiec6BwjAiXJtn_PRll8Z60jpMMB0IQX6ar-cpM6Tun7-EzfWy';
-    var refresh = null;
 
 app.get('/', function(req, res) {
     var authURL = picasa.getAuthURL(config)
@@ -36,12 +31,17 @@ app.get('/callback', function(req, res) {
    console.log('code is:',req.query);
    var code = req.query.code;
    picasa.getAccessToken(config, code, (error, accessToken, refreshToken) => {
-    console.log('access:',error, accessToken, refreshToken);
-    var options = {}
+    console.log('access:', accessToken );
+    console.log('refresh',refreshToken);
 
-    picasa.getAlbums(accessToken, options,  (error, albums) => {
-    console.log(error, albums)
-    })
+     config.accessToken =    accessToken;
+     if(refreshToken){
+        config.refreshToken = refreshToken;
+     }
+
+     fs.writeFileSync("./config.json", JSON.stringify(config));
+
+    res.send('access:'+ accessToken +"<br>"+'refresh'+refreshToken);
 
     })
 
@@ -61,10 +61,10 @@ app.get('/albums', function(req, res) {
 app.get('/upload', function(req, res) {
     var  albumId = '5796511539998971217'
 
-    fs.readFile(__dirname + '/photos/jake.jpg', (err, binary) => {
+    fs.readFile(__dirname + '/photos/album1/IMG_4459.JPG', (err, binary) => {
     const photoData = {
-        title       : 'Jake the dog',
-        summary     : 'Corgis ftw!',
+        title       : 'zeinab-title',
+        summary     : 'zeinab-summary',
         contentType : 'image/jpeg',
         binary      : binary
     }
--- looper.js
+++ looper.js
@@ -1,25 +1,43 @@
 var fs = require('fs');
 
+var Picasa = require('picasa')
+var picasa = new Picasa();
+
+ var dataConfig = fs.readFileSync('./config.json', 'utf8');
+ var config = JSON.parse(dataConfig); //now it an object
+console.log(config);
+
+
+var options = {}
+
+// var getAlbum = function(){
+//     picasa.getAlbums(config.accessToken, options,  (error, albums) => {
+//     console.log('dddddddd');
+// // console.log(error, albums);
+//     if(error.statusCode == 403){
+//         console.log('refresh');
+//         getAlbum();
+//     }
+
+//     });
+// };
+// getAlbum();  
+
+
 var walkPath = './photos';
+var logPath = './logs/';
 
-var exists = function(albumName){
-    return fs.stat('foo.txt', function(err, stat) {
-        if(err == null) {
-            console.log('File exists');
-
-            return 'complete' || 'pending';
-        } else if(err.code == 'ENOENT') {
-            // file does not exist
-            return false;
-            // fs.writeFile('log.txt', 'Some log\n');
-        } else {
-            console.log('Some other error: ', err.code);
-        }
-    });
+var readLog= function(albumName){
+         
+           var data = fs.readFileSync(logPath+albumName+'.txt', 'utf8');
+           obj = JSON.parse(data); //now it an object
+           return obj;
 }
-
+        var lastDir = null;
 var walk = function (dir, done) {
+    console.log('walk:',dir);
     fs.readdir(dir, function (error, list) {
+        console.log("list:",list);
         if (error) {
             return done(error);
         }
@@ -32,39 +50,113 @@ var walk = function (dir, done) {
             if (!file) {
                 return done(null);
             }
-            
+            var fileName = file;
             file = dir + '/' + file;
             
             fs.stat(file, function (error, stat) {
         
                 if (stat && stat.isDirectory()) {
-                    // create album if there is no and save the id in a json
-                    if(!exists(albumName)){
-                        // create album and create a json
-                    }else if('complete'){
-                            // ignore it
-                    }else if('pending'){
-                            // continue the remaining pics
-                    }
+                          //// start checking
+                            try {
+                            var stat = fs.statSync(logPath+list[i-1]+'.txt');
+                            var obj =readLog(list[i-1]);
+                             var existed = obj.status;
+                            console.log('it exists');
+                            }
+                            catch(err) {
+                                console.log('it does not exist');
+                                var existed =false;
+                                
+                            }
 
-                    walk(file, function (error) {
-                        next();
-                    });
+                          //// end of checking
+                     
+                          console.log('existing for',list[i-1],' = ',existed);
+                            if(existed != 'complete'){
+                                if(!existed){
+                                    console.log('Creating album with the name of '+list[i-1]);
+                                         var obj = {
+                                                status: 'pending',
+                                                photos:[],
+                                                name: list[i-1],
+                                        };
+                                        fs.writeFileSync(logPath+list[i-1]+".txt", JSON.stringify(obj));
+                                        
+                                        walk(file, function (error) {
+                                            next();
+                                        });
+                                    
+                                }else{ // for pending we just walk through it
+                                    walk(file, function (error) {
+                                        next();
+                                        });
+                                }
+
+                            }else{
+                                next();
+                            }
+
+                   
                 } else {
+                    // check there is not in json
                     // extract name and album and upload photo to it
                     //save to log file 
-                    fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
-                        if (err){
-                            console.log(err);
-                        } else {
-                        obj = JSON.parse(data); //now it an object
-                        obj.table.push({id: 2, square:3}); //add some data
-                        json = JSON.stringify(obj); //convert it back to json
-                        fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back 
-                    }});
+                    var logNameArray = file.split('/'); 
+                    var obj =readLog(logNameArray[2]);
+                    if(obj.photos.indexOf(list[i-1])<0){
+                        
 
-                    console.log(file);
-                    next();
+                        console.log('upload image with the name of '+list[i-1]+' in '+logNameArray[2]); 
+                        
+                        //////////////////
+                        
+                        var path = __dirname + "/photos/"+logNameArray[2]+"/"+list[i-1];
+                        console.log('path:',path);
+                        fs.readFile(path, (err, binary) => {
+                            var photoData = {
+                                title       : list[i-1],
+                                summary     : logNameArray[2],
+                                contentType : 'image/jpeg',
+                                binary      : binary
+                            }
+                           function postImage(){
+                                picasa.postPhoto(config.accessToken, config.albumId, photoData, (error, response) => {
+                                console.log('uploaded:',error, response);
+                                if(error){
+                                    if(error.statusCode == 403){
+                                        //refresh the accessToken and try again
+                                        console.log('refresh token');
+
+                                        picasa.renewAccessToken(config, config.refreshToken, (error, accessToken) => {
+                                            console.log('new ',error, accessToken);
+                                            if(!error){
+                                                config.accessToken =    accessToken;
+                                                fs.writeFileSync("./config.json", JSON.stringify(config));
+                                                postImage();
+                                            }
+                                            
+                                        })
+                                    }
+                                }else{
+                                    var logNameArray = file.split('/'); 
+                                    var obj =readLog(logNameArray[2]);
+                                    obj.photos.push(list[i-1]);
+                                    if(i==list.length){
+                                        obj.status = "complete";
+                                    }
+                                    fs.writeFileSync(logPath+logNameArray[2]+".txt", JSON.stringify(obj));
+
+                                }
+                                                                
+                                next();
+
+                            })
+                           }
+                           postImage();
+                            
+                        })
+                        //////////////////
+                    }
                 }
             });
         })();
